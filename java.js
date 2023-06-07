988105a55f0a991445b46ec54add4d25c7b7cb29
ShilaMsengi <135307619+ShilaMsengi@users.noreply.github.com>
2023-06-07 15:30:11 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,14 +1,10 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    setInterval(getSensorValues, 5000); // Fetch sensor values every 5 seconds
-  });
-  
-  function getSensorValues() {
-    fetch('https://msqtrial-5pjfhzlk0-shilamsengi.vercel.app/') // Replace with your actual URL
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('value1').textContent = data.value1;
-        document.getElementById('value2').textContent = data.value2;
-      })
-      .catch(error => console.error('Error:', error));
-  }
-  
+const apiKey = YourApiKey;
+const accessKey = YourAccessKey;
+const accessToken = YourAccessToken;
+
+
+const project = grandeur.init(apiKey, accessKey, accessToken);
+project.auth().login(YourUserEmail, YourUserPassword);
+
+// This subscribes to the "millis" variable.
+project.devices().device(YourDeviceId).data().on("millis", (path, value) => document.getElementById("data").innerHTML = value);
