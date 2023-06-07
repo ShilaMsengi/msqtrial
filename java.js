d5a70cdfbc71680dd2e62cad74bd9316110abd6e
ShilaMsengi <135307619+ShilaMsengi@users.noreply.github.com>
2023-06-07 16:47:17 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,7 +1,8 @@
-const apiKey = YourApiKey;
-const accessKey = YourAccessKey;
-const accessToken = YourAccessToken;
-
+const apiKey = "grandeurlildnbn804990ji402ll13t7";
+const accessKey = "c8b8b67dd919344834650127d9ec321fca79f46dd01b514477b45ea0ec53ac27";
+const accessToken = "cac4db8cac536eec83693f973a42caa5d30d07fdff8e0dc51e38523462ff97f8";
+const YourUserEmail = "shilamsengi23@gmail.com"; 
+const YourUserPassword = "babashila"; 
 
 const project = grandeur.init(apiKey, accessKey, accessToken);
 project.auth().login(YourUserEmail, YourUserPassword);
